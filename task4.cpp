3adde0b857dd9a97e78a0ccfb7081c6bfec12fa6
Inshal-123 <inshalshafiq285@gmail.com>
2023-09-17 00:51:17 -0700

    message

---
+++ task4.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"      ####                      \n";
+cout<<"     ##  ##                     \n";
+cout<<"     ######                       \n";
+cout<<"     ##  ##                     \n";
+cout<<"     ##  ##                     \n";
+cout<<"                            \n";
+cout<<"    ##   ##                     \n";
+cout<<"    ##   ##                     \n";
+cout<<"    #####                        \n";
+cout<<"    ##                        \n";
+cout<<"    #####                        \n";
+cout<<"    ##   ##                     \n";
+cout<<"    ##   ##                     \n";
+cout<<"                            \n";
+cout<<"    #######                       \n";
+cout<<"         ##                   \n";
+cout<<"        ##                    \n";
+cout<<"       ##                     \n";
+cout<<"      ##                      \n";
+cout<<"     ##                       \n";
+cout<<"    #######                        \n";
+cout<<"                            \n";
+cout<<"    ##    ##                    \n";
+cout<<"    ##   ##                     \n";
+cout<<"    ##  ##                      \n";
+cout<<"    ####                        \n";
+cout<<"    ###                        \n";
+cout<<"    ####                        \n";
+cout<<"    ##  ##                      \n";
+cout<<"    ##   ##                     \n";
+cout<<"    ##    ##                    \n";
+cout<<"                            \n";
+cout<<"      ####                      \n";
+cout<<"     ##  ##                     \n";
+cout<<"     ##                       \n";
+cout<<"      ####                      \n";
+cout<<"          ##                  \n";
+cout<<"      ##  ##                  \n";
+cout<<"       ####                      \n";
+
+}
---
+++ task9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"  ------------------------------------------   \n";
+cout<<"        o   ^__^                      \n";
+cout<<"          o (oo)\\____________                     \n";
+cout<<"            (__)\\            )\\/\\    \n";
+cout<<"                ||          ||      \n";
+cout<<"                ||----------||   \n";
+cout<<"                ||          ||   \n";
+}
